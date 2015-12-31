af21bb77746f3211cf34f3ecbabdd0b6bb5270be
TM-KNYM <mushimushikera@gmail.com>
2015-12-31 10:55:12 +0900

    add fisheye demo

--- vrsandbox/js/Main.js
+++ vrsandbox/js/Main.js
@@ -13,9 +13,9 @@
 	function init() {
 		scene = new THREE.Scene();
 
-		camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 9000);
-		camera.position.set(0, -100, 0);
-		camera.up.set(0, 1, 0);
+		camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 9000);
+		camera.position.set(0, -100, -200);
+		camera.up.set(0, 0, 1);
 		scene.add(camera);
 
 		renderer = new THREE.WebGLRenderer({antialias: true});
@@ -33,19 +33,19 @@
 
 		effect = new THREE.StereoEffect(renderer);
 
-		controls = new THREE.OrbitControls(camera, element);
+		/*controls = new THREE.OrbitControls(camera, element);
 		controls.rotateUp(Math.PI / 4);
 		controls.target.set(
 			camera.position.x + 0.15,
 			camera.position.y,
 			camera.position.z
 		);
-		controls.noZoom = true;
+        controls.noZoom = true;
 		controls.noPan = true;
-
+*/
 		clock = new THREE.Clock();
 
-		window.addEventListener("deviceorientation", setOrientationControls, true);
+		//window.addEventListener("deviceorientation", setOrientationControls, true);
 
 		window.addEventListener("resize", resize, false);
 		resize();
@@ -72,17 +72,13 @@
 		window.removeEventListener("deviceorientation", setOrientationControls, true);
 	}
 
-	/**
-	 * 立方体の生成します。
-	 */
 	function createBox() {
 		edgesPool = [];
 		var geometry = new THREE.SphereGeometry(STEP);
 		for(var i = 0; i < 1000; i++) {
-            var cr = Math.round(0xFFFFFF * (Math.random()-0.5));
+            var cr = 0xFFFFFF;
             var material = new THREE.MeshBasicMaterial({ color: cr });
 			var egh = new THREE.Mesh(geometry, material);
-			//var egh = new THREE.EdgesHelper(mesh, color);
 			egh.updateMatrix();
 			scene.add(egh);
 			edgesPool.push(egh);
@@ -95,16 +91,16 @@
 	 */
 	function startDrop(egh) {
 		egh.position.x = 0; // Math.round(9000 * (Math.random() - 0.5) / STEP);
-		egh.position.z = 0; // Math.round(9000 * (Math.random() - 0.5) / STEP) ;
-		egh.position.y = -20000;//STEP * Math.round(9000 * (Math.random() - 0.5) / STEP) ;
+		egh.position.z = -20000; // Math.round(9000 * (Math.random() - 0.5) / STEP) ;
+		egh.position.y = 0;//STEP * Math.round(9000 * (Math.random() - 0.5) / STEP) ;
 		egh.updateMatrix();
 
 		// 秒数
 		var sec = 2 * Math.random() + 3;
 		TweenMax.to(egh.position, sec, {
 			x: Math.round(9000 * (Math.random() - 0.5) / STEP),
-			z: Math.round(9000 * (Math.random() - 0.5) / STEP),
-			y: 0,
+			y: Math.round(9000 * (Math.random() - 0.5) / STEP),
+			z: 0,
 			ease: Expo.easeOut,
 			onComplete: endDrop,
 			onCompleteParams: [egh]
@@ -155,8 +151,7 @@
 		}
 
 		camera.updateProjectionMatrix();
-
-		controls.update(dt);
+		//controls.update(dt);
 	}
 
 	/**
